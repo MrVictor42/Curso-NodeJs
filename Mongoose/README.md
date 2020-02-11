8bafef6a2ffc9f327377944e3294e3ac654a6103
MrVictor42 <guin-hu@hotmail.com>
2020-02-11 18:26:12 -0300

    Register data in moongoose

--- Mongoose/README.md
+++ Mongoose/README.md
@@ -1,3 +1,3 @@
-<h2> Class 29 - Started Mongoose  </h2>
+<h2> Class 30 - Working with Mongoose  </h2>
 
-* Install Mongoose: npm i --save mongoose;
+* Models in moongoose;
--- Mongoose/learning.js
+++ Mongoose/learning.js
@@ -2,9 +2,45 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/learning', {
-    useMongoClient: true
 }).then(() =>{
     console.log('MongoDB connected')
 }).catch((error) =>{
     console.log('Error:' + error)
+});
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true 
+    },
+    last_name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    age: {
+        type: Number,
+        require: true
+    },
+    country: {
+        type: String,
+    }
+});
+
+mongoose.model('users', UserSchema);
+const user = mongoose.model('users');
+
+new user ({
+    name: 'Victor',
+    last_name: 'Mota',
+    email: 'victor@gmail.com',
+    age: 24,
+    country: 'Brazil'
+}).save().then(() => {
+    console.log('User save with success!');
+}).catch((error) =>{
+    console.log('Error: ' + error);
 });
