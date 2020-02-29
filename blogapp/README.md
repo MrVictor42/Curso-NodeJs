4b6ac428270f123cb752ba363b33bad3e055c2c1
MrVictor42 <guin-hu@hotmail.com>
2020-02-29 12:46:14 -0300

    Methods for register users and add a new warning_msg in _msg.handlebars

--- blogapp/README.md
+++ blogapp/README.md
@@ -1,1 +1,5 @@
-<h2> Class 55 - Model user  </h2>
+<h2> Class 56 - Register user in database  </h2>
+
+* npm i --save bcryptjs;
+* The bcryptjs is used for encrypt password;
+* Insert a new warning_msg in _msg.handlebars;
--- blogapp/routes/users.js
+++ blogapp/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 require('../models/User');
 const User = mongoose.model('users');
+const bcrypt = require('bcryptjs');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -27,14 +28,46 @@ router.post('/register', (req, res) => {
         errors.push({text: 'Insert a password bigger than 4 letters!'})
     }
 
-    if(req.body.password !== !req.body.password2) {
+    if(req.body.password !== req.body.password2) {
         errors.push({text: 'Passwords is differents! Please put same passoword in both fields!'});
     }
 
     if(errors.length > 0) {
         res.render('users/register', {errors: errors});
     } else {
-        
+        User.findOne({email: req.body.email}).then((user) => {
+            if(user) {
+                req.flash('warning_msg','Already exist one account with this email in our system!');
+                res.redirect('/users/register');
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password 
+                });
+
+                bcrypt.genSalt(10, (error, salt) => {
+                    bcrypt.hash(newUser.password, salt, (error, hash) => {
+                        if(error) {
+                            req.flash('error_msg', 'Had a error during the save of user');
+                            res.redirect('/');
+                        }
+                        newUser.password = hash
+                        newUser.save().then(() => {
+                            req.flash('success_msg', 'User save with success!');
+                            res.redirect('/');
+                        }).catch((error) => {
+                            req.flash('error_msg', 'Had a error in create account, try again!');
+                            res.redirect('/users/register');
+                        });
+                    });
+                });
+            }
+        }).catch((error) => {
+            req.flash('error_msg', 'Had a intern error: ' + error);
+            res.redirect('/');
+        });
+
     }
 });
 
